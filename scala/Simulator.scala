c5c4f67316e453b4ddcd360ff33ee1476ee419f9
AlSzacrel <al@alszacrel.de>
2014-10-19 12:26:02 +0200

    Added CSV status, fixed statistic collection of spending targets greater than wallet content, added Simulation descriptor

--- scala/Simulator.scala
+++ scala/Simulator.scala
@@ -1,6 +1,6 @@
 import util.Random
 
-class Simulator(utxo: Set[(Int,Long)], operations: List[Long]) {
+class Simulator(utxo: Set[(Int,Long)], operations: List[Long], descriptor: String) {
     val regularWallet = new Wallet("Regular", utxo,false, 1000) 
     val pruningWallet = new PruningWallet("Pruning", true, 1, utxo,false, 1000) 
     val pruningWalletMin2 = new PruningWallet("Pruning 2Min", true, 2, utxo,false,1000) 
@@ -20,7 +20,9 @@ class Simulator(utxo: Set[(Int,Long)], operations: List[Long]) {
         println("-------------------------------------------------------------------------------")
         println("---------------- FINAL RESULTS ------------------------------------------------")
         println("-------------------------------------------------------------------------------")
-        wallets.foreach(_.printWalletStatus())
+        println(descriptor)
+        wallets.head.printStatusHeader()
+        wallets.foreach(_.printWalletStatusCSV())
         println("-------------------------------------------------------------------------------")
     }
 }
@@ -53,16 +55,16 @@ class ParetoGenerator (low:Int, high:Int, k:Int) {
 object Simulation {
     def main(args: Array[String]) = {
 
-        val testCases = new Simulator(Set(), List(1, -1, 1, 1, -2, 1,1,1, 10, -9, -4, 3,3,3,3,11,-10, -1, -8))
-        val testCases2 = new Simulator(Set(), List(2, 2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2, 9,-11))
-        val testCases3 = new Simulator(Set(), List(2, 2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2, -11))
+        val testCases = new Simulator(Set(), List(1, -1, 1, 1, -2, 1,1,1, 10, -9, -4, 3,3,3,3,11,-10, -1, -8),"TESTCASE 1: Check of Functionality")
+        val testCases2 = new Simulator(Set(), List(2, 2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2, 9,-11), "TESTCASE 2: Direct Match Test")
+        val testCases3 = new Simulator(Set(), List(2, 2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2, -11), "TESTCASE 3: Pruning Test, Direct Match impossible")
         
         var utxo4: Set[(Int,Long)] = Set()
         for(i <- 1 to 50000) {
             var utxo : (Int, Long) = (i,i.toLong)
             utxo4 = utxo4 + utxo
         }
-        val testCases4 = new Simulator(utxo4, List(-50001))
+        val testCases4 = new Simulator(utxo4, List(-50001), "TESTCASE 4: UTXO 1-50000, spend 50001")
 
         var utxo5: Set[(Int,Long)] = Set()
         for(i <- 1 to 20001) {
@@ -75,7 +77,7 @@ object Simulation {
 
             utxo5 = utxo5 + utxo
         }
-        val testCases5 = new Simulator(utxo5, List(-50000))
+        val testCases5 = new Simulator(utxo5, List(-50000), "TESTCASE 5: 20000 * 1 satoshi, and two that make up spending target of 50000")
 
         var utxo6: Set[(Int,Long)] = Set()
         var ops6: List[Long] = List()
@@ -94,7 +96,7 @@ object Simulation {
             }
             ops6 = nextOp :: ops6
         }
-        val testCases6 = new Simulator(utxo6, ops6)
+        val testCases6 = new Simulator(utxo6, ops6, "TESTCASE 6: Start with 5000 random UTXO from (2500,250000) satoshi, 10000 operations of spending or receiving (540,250000) satoshi")
 
         var utxo7: Set[(Int,Long)] = Set()
         var ops7: List[Long] = List()
@@ -113,7 +115,7 @@ object Simulation {
             }
             ops7 = nextOp :: ops7
         }
-        val testCases7 = new Simulator(utxo7, ops7)
+        val testCases7 = new Simulator(utxo7, ops7, "TESTCASE 7: Start with 1000 random UTXO from (2500 satoshi,0.1BTC), 10000 operations of 50-50 spending or receiving with Gaussian distribution (540 satoshi,2.5BTC).")
         
         var utxo8: Set[(Int,Long)] = Set()
         var ops8: List[Long] = List()
@@ -133,7 +135,7 @@ object Simulation {
             }
             ops8 = nextOp :: ops8
         }
-        val testCases8 = new Simulator(utxo8, ops8)
+        val testCases8 = new Simulator(utxo8, ops8, "TESTCASE 8: Start with 1000 random UTXO from (2500 satoshi,0.1BTC), 10000 operations of 1-3 spending or receiving with Pareto distribution (540 satoshi,1BTC).")
 println("--------------------------------------------------------------------------------------")
 println("---------------TEST CASE 1 STARTING---------------------------------------------------")
 println("--------------------------------------------------------------------------------------")
